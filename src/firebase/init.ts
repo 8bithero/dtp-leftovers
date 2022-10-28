8351d49430fc4784070beb26e3c16d95c9b24aea
Paris Kemanes <pkemanes@gmail.com>
2022-10-28 21:06:57 +0100

    Removed Firebase analytics

--- src/firebase/init.ts
+++ src/firebase/init.ts
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-import { getAnalytics, logEvent } from 'firebase/analytics';
 import { getFirestore } from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -18,10 +17,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+initializeApp(firebaseConfig);
 const db = getFirestore();
-
-logEvent(analytics, 'send_page_view');
 
 export default db;
