15bd6cf325e5f2dd4af0120012a654f0169dc0e4
Paris Kemanes <pkemanes@gmail.com>
2022-10-29 10:36:46 +0100

    Changef netlify function - keeps timing out

--- netlify/functions/scheduledMintCheck.ts
+++ netlify/functions/scheduledMintCheck.ts
@@ -1,11 +1,14 @@
 import { schedule, Handler, HandlerEvent } from '@netlify/functions';
+import { reactive } from 'vue';
 import { useMintAddressUpdater } from '../../src/composables/useMintAddressUpdater';
+import { usePandaData } from '../../src/composables/usePandaData';
 
 export const handler: Handler = schedule(
   '*/3 * * * *',
   async (event: HandlerEvent) => {
-    const hasUpdates = await useMintAddressUpdater();
-
+    const pandas = reactive(usePandaData());
+    const initialNFTs = await pandas.getAll();
+    const hasUpdates = await useMintAddressUpdater(initialNFTs);
     const statusCode = hasUpdates ? 200 : 304;
 
     const eventBody = JSON.parse(event.body);
